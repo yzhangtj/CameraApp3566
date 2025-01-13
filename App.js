596fcf645f41d3eba4e024b1f8eb4787a02d2279
Kyle Jiang <kylejoeca@gmail.com>
2025-01-13 15:22:12 +0800

    update on permission and cam snapshot

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ export default function App() {
   const [photoBinary, setPhotoBinary] = useState(null); // Photo as binary data
   const [audioBinary, setAudioBinary] = useState(null); // Audio as binary data
   const [isRecording, setIsRecording] = useState(false);
+  const [hasMicPermission, setHasMicPermission] = useState(false);
   const cameraRef = useRef(null); // Reference for the Camera
 
   useEffect(() => {
@@ -33,6 +34,7 @@ export default function App() {
         console.error('Microphone permission was denied');
         return;
       }
+      setHasMicPermission(true);
   
       console.log('All permissions granted');
     } catch (err) {
@@ -42,45 +44,46 @@ export default function App() {
 
   const capturePhotoAndAudio = async () => {
     try {
-      if (isRecording) {
-        // Stop audio recording
-        console.log('Stopping audio recording...');
-        const result = await audioRecorderPlayer.stopRecorder();
-        setIsRecording(false);
-
-        // Read audio file as binary (base64)
-        const audioBinary = await RNFS.readFile(result, 'base64');
-        setAudioBinary(audioBinary);
-        console.log('Audio binary (base64):', audioBinary);
-      } else {
-        // Ensure the camera reference is valid
-        if (!cameraRef.current) {
-          console.error('Camera reference is null . Cannot capture photo.');
-          return;
+      if (hasPermission && hasMicPermission) {
+        if (isRecording) {
+          // Stop audio recording
+          console.log('Stopping audio recording...');
+          const result = await audioRecorderPlayer.stopRecorder();
+          setIsRecording(false);
+  
+          // Read audio file as binary (base64)
+          const audioBinary = await RNFS.readFile(result, 'base64');
+          setAudioBinary(audioBinary);
+          console.log('Audio binary (base64):', audioBinary);
+        } else {
+          // Ensure the camera reference is valid
+          if (!cameraRef.current) {
+            console.error('Camera reference is null . Cannot capture photo.');
+            return;
+          }
+  
+          // Capture photo and get base64 binary data
+          try {
+            console.log('Capturing photo...');
+            const photo = await cameraRef.current.takeSnapshot({
+              quality: 0.5,
+              base64: true,
+            })
+            console.log('Get photo in raw: ', photo);
+            // Set the photo binary data
+            const img = await RNFS.readFile(photo.path, 'base64');
+            console.log('IMG binary (base64):', img);
+          } catch (error) {
+            console.error('Error capturing photo:', error);
+            setErrorMessage(`Error:${error.message}`);
+          }
+  
+          // Start audio recording
+          console.log('Starting audio recording...');
+          const audioUri = `${RNFS.DocumentDirectoryPath}/audio.m4a`;
+          await audioRecorderPlayer.startRecorder(audioUri);
+          setIsRecording(true);
         }
-
-        // Capture photo and get base64 binary data
-        try {
-          console.log('Capturing photo...');
-          const photo = await cameraRef.current.takePhoto({
-            qualityPrioritization: 'speed',
-            base64: true, // Return photo as base64
-          });
-          console.log('Get photo in binary');
-          // Set the photo binary data
-          const photoBinary = photo.base64;
-          setPhotoBinary(photoBinary);
-          console.log('Photo binary (base64):', photoBinary);
-        } catch (error) {
-          console.error('Error capturing photo:', error);
-          setErrorMessage(`Error:${error.message}`);
-        }
-
-        // Start audio recording
-        console.log('Starting audio recording...');
-        const audioUri = `${RNFS.DocumentDirectoryPath}/audio.m4a`;
-        await audioRecorderPlayer.startRecorder(audioUri);
-        setIsRecording(true);
       }
     } catch (err) {
       console.error('Error during photo or audio capture:', err);
@@ -92,6 +95,14 @@ export default function App() {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>No access to camera</Text>
+      </View>
+    );
+  }
+  
+  if (!hasMicPermission) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>No access to mic</Text>
       </View>
     );
   }
